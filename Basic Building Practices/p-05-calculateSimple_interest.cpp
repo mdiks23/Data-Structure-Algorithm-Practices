6ad48f77409280b8cca284f310b3189893283ffa
MD. IMTIAZ KHONDOKAR SUPTO <158520611+mdiks23@users.noreply.github.com>
2025-03-10 22:46:32 +0600

    Add files via upload

--- Basic Building Practices/p-05-calculateSimple_interest.cpp
+++ Basic Building Practices/p-05-calculateSimple_interest.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 int main(){
   int money;
-  int roi;
-  int year;
+  float roi;
+  float year;
   cout<<"Enter The Amount Of Money : "<<endl;
   cin>>money;
   cout<<"Enter The Rate Of Interest : "<<endl;
   cin>>roi;
   cout<<"Enter The Time Period in Years : "<<endl;
   cin>>year;
-  int interest= (money*year*roi)/100;
+ float interest= (money*year*roi)/(double)100;
   cout<<"Your Net Interest Amount Is : "<< interest <<endl;
 }
