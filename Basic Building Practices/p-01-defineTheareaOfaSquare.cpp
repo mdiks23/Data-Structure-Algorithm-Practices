19d6b58503a887d03c929c7f8087e75935a5702d
MD. IMTIAZ KHONDOKAR SUPTO <158520611+mdiks23@users.noreply.github.com>
2025-03-11 20:58:08 +0600

    Delete Basic Building Practices directory

--- Basic Building Practices/p-01-defineTheareaOfaSquare.cpp
+++
@@ -1,9 +0,0 @@
-//Qs: Let the length of a side of a square is 50units. Then define the area of it.
-#include <iostream>
-using namespace std ;
-int main () {
-  int length = 50;
-  int area = length*length;
-  cout<<"The Area of the square is : "<< area <<"squnits"<< endl;
-  return 0;
-}
