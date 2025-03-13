cf85e4241c17ab878194693734ba6a1ea864e1e8
MD. IMTIAZ KHONDOKAR SUPTO <158520611+mdiks23@users.noreply.github.com>
2025-03-14 04:41:11 +0600

    Add files via upload

---
+++ Basics of DSA/Patterns & Nested Loops/p-04-Floyd's TriAngle/Floyd's-02.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int n=5;
+  char num='A';
+  for(int i=0; i<n; i++){
+  for(int j=1; j<=i+1; j++){
+  cout<< num <<" ";
+  num++;
+  }
+  cout<<endl;
+  }
+  return 0;
+}
